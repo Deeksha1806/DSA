f21b78be4e25bc530102699fa67ab0317fcc96bb
Deeksha1806 <deekshanarisetty@gmail.com>
2026-01-31 15:03:50 +0530

    Time: 15 ms (78.08%) | Memory: 19.8 MB (16.38%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -2,4 +2,4 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         c1=Counter(ransomNote)
         c2=Counter(magazine)
-        return not c1-c2
+        return  not c1-c2
